ade915c62c899dd50a9852e1908b20b98e1d5401
NhatTan <tandaikaa123@gamil.com>
2021-11-17 15:31:44 +0700

    Tân 15:30 17/11/2021

--- app/src/main/java/nhattan/lnt/clothesshop/ContactActivity.java
+++ app/src/main/java/nhattan/lnt/clothesshop/ContactActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class ContactActivity extends AppCompatActivity {
 
     ImageButton ibtn_Exit;
+    EditText edt_Tentaikhoan, edt_Sdt, edt_NoiDunggopy;
+    Button btn_Gopy, btn_Thoat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +24,26 @@ public class ContactActivity extends AppCompatActivity {
     }
 
     private void Anhxa() {
+        edt_Tentaikhoan = findViewById(R.id.edtTaikhoangopy);
+        edt_Sdt = findViewById(R.id.edtSdtgopy);
+        edt_NoiDunggopy = findViewById(R.id.edtNoidunggopy);
+        btn_Gopy = findViewById(R.id.btnGopy);
+
+        btn_Thoat = findViewById(R.id.btnThoatgopy);
+        btn_Thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ContactActivity.this, HomeActivity.class));
+            }
+        });
+
         ibtn_Exit = findViewById(R.id.ibtnExit);
         ibtn_Exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getExit();
+                startActivity(new Intent(ContactActivity.this, HomeActivity.class));
             }
         });
     }
 
-    private void getExit()
-    {
-        Intent iExit = new Intent(ContactActivity.this, HomeActivity.class);
-        startActivity(iExit);
-    }
 }
